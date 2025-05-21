5dd971fbb42bad2efbbbf782a68e74417716bddb
ritty65 <contact@simulacraproductions.net>
2025-05-21 21:25:59 +0200

    Add key manager component and page

---
+++ src/pages/KeyManagerPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import KeyManager from '../components/KeyManager';
+
+export default function KeyManagerPage() {
+  return (
+    <div>
+      <KeyManager />
+    </div>
+  );
+}
