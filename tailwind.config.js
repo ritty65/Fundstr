f7db495987562a8113cd377589c533a6bff27c7c
ritty65 <contact@simulacraproductions.net>
2025-08-25 20:00:48 +0200

    feat: bundle tailwind for creator search

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./public/find-creators.html",
+  ],
   theme: {
     extend: {
       fontFamily: {
